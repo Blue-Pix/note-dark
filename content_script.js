23e1efbb6f5f47fb3d207fed69a4d09e59ccd482
sakuraya <sakuraya@blue-pixel.info>
2020-04-06 21:40:59 +0900

    update

--- content_script.js
+++ content_script.js
@@ -10,6 +10,9 @@ function replaceLogo(e) {
   if(img.length > 0) {
     img[0].src = logoFile;
   }
+  let img2 = $('img.o-header__logoImage');
+  if(img2.length > 0) {
+    img2[0].src = logoFile;
+  }
   $('h2.logo').css('background-image', 'url(' + logoFile + ')');
 }
-
